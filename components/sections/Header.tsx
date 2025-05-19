434bc8c3fd03533765a545d11a7e455a3c462e0f
Bora Deniz Bas <boradenizbas1@gmail.com>
2025-05-20 02:55:43 +0300

    header links/styling

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -5,6 +5,10 @@ import { FaXTwitter } from "react-icons/fa6";
 import { IoLogoGithub } from "react-icons/io";
 import { FaInstagram } from "react-icons/fa6";
 import { MessageCircle, Phone } from 'lucide-react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Herr_Von_Muellerhoff } from 'next/font/google';
+import { getHmrRefreshHash } from 'next/dist/server/app-render/work-unit-async-storage.external';
 
 
 
@@ -22,6 +26,26 @@ const Header = () => {
             href: "#", icon: <FaInstagram />
         }
     ]
+    const navigationLinks = [
+        {
+            href: "/", label: "Home"
+        },
+        {
+            href: "/about", label: "About"
+        },
+        {
+            href: "/contact", label: "Contact"
+        },
+        {
+            href: "/hotels", label: "Hotels"
+        },
+        {
+            href: "/rent-a-cars", label: "Rent A Car"
+        },
+        {
+            href: "/trips", label: "Trips"
+        }
+    ]
     return (
         <header className='bg-black text-white'>
             <div className='flex container mx-auto h-16 justify-center md:justify-between items-center px-4 py-2 text-sm'>
@@ -29,21 +53,48 @@ const Header = () => {
                     <div className='flex gap-3 items-center'>
                         <div className='bg-white p-2 rounded-full'>
                             <MessageCircle size={12} className='text-orange-500' />
-
                         </div>
                         info@trabbel.com
                     </div>
-
                     <div className='flex gap-3 items-center'>
                         <div className='bg-white p-2 rounded-full'>
                             <Phone size={12} className='text-orange-500' />
                         </div>
                         123 123 13 12
                     </div>
+                </div>
+                <div className='hidden md:flex items-center space-x-4'>
+                    {
+                        socialLinks.map((e, index) => (
+                            <Link key={index} href={e.href} className='hover:text-orange-500'>
+                                {e.icon}
+                            </Link>
+                        ))
+                    }
+                </div>
+            </div>
+            {/**nav bar */}
+            <div className='bg-white h-28 text-black shadow flex items-center'>
+                <div className='container mx-auto px-4 py-3 flex justify-between items-center'>
+                    <Image
+                        src={"/logo.png"}
+                        alt='travel'
+                        width={210}
+                        height={50}
+                        className='w-36 lg:w-52 h-auto'
+                    />
+                    <nav className='hidden lg:flex space-x-8 text-lg font-semibold'>
+                        {
+                            navigationLinks.map((e, index) => (
+                                <Link href={e.href} key={index}>{e.label}</Link>
+                            ))
+                        }
+                    </nav>
 
 
                 </div>
             </div>
+
         </header>
     )
 }
