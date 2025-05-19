e62b2bd90a8ba1fc7765289149d36f7eedef6d8a
Bora Deniz Bas <boradenizbas1@gmail.com>
2025-05-19 04:50:54 +0300

    styling header

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -1,8 +1,50 @@
+
 import React from 'react'
 
+import { FaXTwitter } from "react-icons/fa6";
+import { IoLogoGithub } from "react-icons/io";
+import { FaInstagram } from "react-icons/fa6";
+import { MessageCircle, Phone } from 'lucide-react';
+
+
+
+
+
 const Header = () => {
+    const socialLinks = [
+        {
+            href: "#", icon: <FaXTwitter />
+        },
+        {
+            href: "#", icon: <IoLogoGithub />
+        },
+        {
+            href: "#", icon: <FaInstagram />
+        }
+    ]
     return (
-        <header className='bg-black text-white'></header>
+        <header className='bg-black text-white'>
+            <div className='flex container mx-auto h-16 justify-center md:justify-between items-center px-4 py-2 text-sm'>
+                <div className='flex items-center gap-5'>
+                    <div className='flex gap-3 items-center'>
+                        <div className='bg-white p-2 rounded-full'>
+                            <MessageCircle size={12} className='text-orange-500' />
+
+                        </div>
+                        info@trabbel.com
+                    </div>
+
+                    <div className='flex gap-3 items-center'>
+                        <div className='bg-white p-2 rounded-full'>
+                            <Phone size={12} className='text-orange-500' />
+                        </div>
+                        123 123 13 12
+                    </div>
+
+
+                </div>
+            </div>
+        </header>
     )
 }
 
