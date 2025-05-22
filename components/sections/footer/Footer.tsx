2e14c4940b9e44a239b2094bf70bae7c80169b78
Bora Deniz Bas <boradenizbas1@gmail.com>
2025-05-22 05:11:51 +0300

    add small animation on footer

--- components/sections/footer/Footer.tsx
+++ components/sections/footer/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
     const instagramImages = Array.from({ length: 11 }, (_, index) => `/instagram/${index + 1}.jpg`)
     return (
         <footer className='relative bg-black text-white '>
-            <div className='absolute -top-52 left-1/2 transform -translate-x-1/2 bg-orange-500 text-left px-6 py-12 rounded-md shadow-lg w-11/12 max-w-6xl h-72 grid grid-cols-1 md:grid-cols-2 items-center gap-6'>
+            <div className='absolute -top-12 left-1/2 transform -translate-x-1/2 bg-orange-500 text-left px-6 py-12 rounded-md shadow-lg w-11/12 max-w-6xl h-72 grid grid-cols-1 md:grid-cols-2 items-center gap-6'>
                 <div>
                     <h2 className='text-3xl font-bold'> Ready to get started?</h2>
                     <p className='mt-2 text-lg'>It only takes a few minutes to register your FREE travel account.</p>
@@ -47,11 +47,16 @@ const Footer = () => {
 
                 </div>
             </div>
-            <div className='container mx-auto py-32 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-7 gap-8'>
-                <div className='lg:col-span-2'>
-                    <h3 className='text-2xl font-bold'>Travel</h3>
+            <div className='container mx-auto py-56 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-7 gap-8'>
+                <div className='lg:col-span-2 mt-16'>
+                    <h4 className='text-2xl font-bold mb-3'>Travel</h4>
+                    <div className='relative w-16 h-1 bg-orange-500 mb-3'>
+                        <div className='absolute top-1/2 left-0 right-0 h-[1px] bg-orange-500'></div>
+                        <div className='absolute h-3 w-3 bg-orange-500 rounded-full top-1/2 -translate-y-1/2 animate-move-dot'></div>
+                        <div className='absolute h-1.5 w-1.5 ml-0.5 bg-white rounded-full top-1/2 -translate-y-1/2 animate-move-dot'></div>
+                    </div>
                     <p className='mt-4 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse illum aliquam voluptatibus impedit sed, minus ut magnam exercitationem rem fuga.</p>
-                    <div className='flex mt-4 space-x-4 text-2xl'>
+                    <div className='flex mt-4 space-x-4 text-2xl '>
                         {
                             socialLinks.map((e, index) => (
                                 <Link key={index} href={e.href} className='hover:text-orange-500'>
@@ -62,8 +67,13 @@ const Footer = () => {
                     </div>
 
                 </div>
-                <div>
+                <div className='lg:col-span-1 mt-16'>
                     <h4 className='text-2xl font-bold mb-4'>Useful Links</h4>
+                    <div className='relative w-16 h-1 bg-orange-500 mb-3'>
+                        <div className='absolute top-1/2 left-0 right-0 h-[1px] bg-orange-500'></div>
+                        <div className='absolute h-3 w-3 bg-orange-500 rounded-full top-1/2 -translate-y-1/2 animate-move-dot'></div>
+                        <div className='absolute h-1.5 w-1.5 ml-0.5 bg-white rounded-full top-1/2 -translate-y-1/2 animate-move-dot'></div>
+                    </div>
                     <div className='space-y-2 text-sm'>
                         {
                             navigationLinks.map((e, index) => (
@@ -72,8 +82,13 @@ const Footer = () => {
                         }
                     </div>
                 </div>
-                <div className='lg:col-span-2'>
-                    <h3 className='text-2xl font-bold mb-4'>Instagram</h3>
+                <div className='lg:col-span-2 mt-16'>
+                    <h4 className='text-2xl font-bold mb-4'>Instagram</h4>
+                    <div className='relative w-16 h-1 bg-orange-500 mb-3'>
+                        <div className='absolute top-1/2 left-0 right-0 h-[1px] bg-orange-500'></div>
+                        <div className='absolute h-3 w-3 bg-orange-500 rounded-full top-1/2 -translate-y-1/2 animate-move-dot'></div>
+                        <div className='absolute h-1.5 w-1.5 ml-0.5 bg-white rounded-full top-1/2 -translate-y-1/2 animate-move-dot'></div>
+                    </div>
                     <div className='grid grid-cols-6 gap-2'>
                         {
                             instagramImages.map((src, index) => (
@@ -88,9 +103,14 @@ const Footer = () => {
                         }
                     </div>
                 </div>
-                <div className='lg:col-span-2'>
+                <div className='lg:col-span-2 mt-16'>
                     <h3 className='text-2xl font-bold mb-4'>Subscribe</h3>
-                    <p className='text-sm mb-4'>subscribe our newsletter for getting quick updates</p>
+                    <div className='relative w-16 h-1 bg-orange-500'>
+                        <div className='absolute top-1/2 left-0 right-0 h-[1px] bg-orange-500'></div>
+                        <div className='absolute h-3 w-3 bg-orange-500 rounded-full top-1/2 -translate-y-1/2 animate-move-dot'></div>
+                        <div className='absolute h-1.5 w-1.5 ml-0.5 bg-white rounded-full top-1/2 -translate-y-1/2 animate-move-dot'></div>
+                    </div>
+                    <p className='text-sm mb-4 mt-8'>subscribe our newsletter for getting quick updates</p>
                     <div className='flex flex-col sm:flex-row'>
                         <Input
                             type='email'
