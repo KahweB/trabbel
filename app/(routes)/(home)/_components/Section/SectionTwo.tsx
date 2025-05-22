e55b3a37adf36e6fecaf884010a2f3a2c0673fec
Bora Deniz Bas <boradenizbas1@gmail.com>
2025-05-22 03:39:39 +0300

    home page sections add

---
+++ app/(routes)/(home)/_components/Section/SectionTwo.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SectionTwo = () => {
+    return (
+        <div className='bg-orange-100 mt-12'>
+            <div className='mx-auto container text-center py-16 px-6 lg:px-28'>
+
+            </div>
+        </div>
+    )
+}
+
+export default SectionTwo
